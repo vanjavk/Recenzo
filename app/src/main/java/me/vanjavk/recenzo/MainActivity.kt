8730c173afa512cc9091051485267aa13607b062
vanjavk <developer@vanjavk.me>
2021-01-23 15:37:10 +0100

    added scanning

--- app/src/main/java/me/vanjavk/recenzo/MainActivity.kt
+++ app/src/main/java/me/vanjavk/recenzo/MainActivity.kt
@@ -1,29 +1,54 @@
 package me.vanjavk.recenzo
 
-import android.annotation.SuppressLint
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Bundle
 import android.os.StrictMode
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import com.google.mlkit.vision.barcode.Barcode
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Detector.Detections
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.serialization.json.Json
 import me.vanjavk.recenzo.model.Product
+import java.io.IOException
 import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var barcodeDetector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+    private var REQUEST_CAMERA_PERMISSION = 201
+
+    //This class provides methods to play DTMF tones
+    private lateinit var toneGen1: ToneGenerator
+    private lateinit var barcodeText: TextView
+    private lateinit var barcodeData: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+        surfaceView = findViewById(R.id.surface_view)
+        barcodeText = findViewById(R.id.barcode_text)
+
+
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
         setupListeners()
+        initialiseDetectorsAndSources()
     }
 
     private fun setupListeners() {
@@ -34,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadData() {
         val jsonResponse = URL("https://privateip1337.lets.ee:8443/product").readText()
         //println(jsonResponse)
-        val obj : Product = Json.decodeFromString(
+        val obj: Product = Json.decodeFromString(
             Product.serializer(),
             jsonResponse
         )
@@ -42,34 +67,72 @@ class MainActivity : AppCompatActivity() {
 
         tvDescription.text = obj.decription
         tvTitle.text = obj.id.toString()
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE,
-                Barcode.FORMAT_EAN_13,
-                Barcode.FORMAT_EAN_8)
+    }
+
+    private fun initialiseDetectorsAndSources() {
+
+        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+        barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
             .build()
-        val scanner = BarcodeScanning.getClient()
-//        val result = scanner.process(image)
-//            .addOnSuccessListener { barcodes ->
-//                // Task completed successfully
-//                // ...
-//            }
-//            .addOnFailureListener {
-//                // Task failed with an exception
-//                // ...
-//            }
-    }
-}
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+        surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        cameraSource.start(surfaceView.holder)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.CAMERA),
+                            REQUEST_CAMERA_PERMISSION
+                        )
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
 
-private class YourImageAnalyzer : ImageAnalysis.Analyzer {
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {}
 
-    @SuppressLint("UnsafeExperimentalUsageError")
-    override fun analyze(imageProxy: ImageProxy) {
-        val mediaImage = imageProxy.image
-        if (mediaImage != null) {
-            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-            // Pass image to an ML Kit Vision API
-            // ...
-        }
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+            override fun receiveDetections(detections: Detections<Barcode>) {
+                val barcodes = detections.detectedItems
+                if (barcodes.size() != 0) {
+                    barcodeText.post {
+                        if (barcodes.valueAt(0).email != null) {
+                            barcodeText.removeCallbacks(null)
+                            barcodeData = barcodes.valueAt(0).email.address
+                            barcodeText.text = barcodeData
+                            toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                        } else {
+                            barcodeData = barcodes.valueAt(0).displayValue
+                            barcodeText.text = barcodeData
+                            toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                        }
+                    }
+                }
+            }
+        })
     }
+
 }
